ea987d0c9f37318bab2ec45361933e8bebb2d01b
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-02-12 20:48:39 -0700

    changed sidebar, added home page

--- produly-app/src/components/Layout.jsx
+++ produly-app/src/components/Layout.jsx
@@ -1,40 +1,42 @@
-import { createContext, useState } from 'react'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import '../App.css'
-import Sidebar from './Sidebar'
+import { createContext, useState } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import "../App.css";
+import Sidebar from "./Sidebar";
+import { useNavigate } from "react-router-dom";
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
-function Layout ({ children }) {
-  const [theme, setTheme] = useState('light')
+function Layout({ children }) {
+  const nav = useNavigate();
+  const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
-  }
-
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div id={theme} className='app'>
-        <div className='header'>
-          <h1 className='title'>produly</h1>
-          <button className='mode-button' onClick={toggleTheme}>
-            {theme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+      <div id={theme} className="app">
+        <div className="header">
+          <h1
+            className="title"
+            onClick={() => {
+              nav("/");
+            }}
+          >
+            produly
+          </h1>
+          <Sidebar />
+          <button className="mode-button" onClick={toggleTheme}>
+            {theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
           </button>
         </div>
-        <div className='main'>
-          <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-          <main className='page-content'>{children}</main>
-        </div>
+        <hr className="header-border"></hr>
+        <main className="page">{children}</main>
       </div>
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- produly-app/src/components/Sidebar.jsx
+++ produly-app/src/components/Sidebar.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
-import { SidebarData } from '../data/SidebarData'
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { SidebarData } from "../data/SidebarData";
 
-const Sidebar = ({ isOpen, setIsOpen }) => {
-  const navigate = useNavigate()
+const Sidebar = () => {
+  const navigate = useNavigate();
 
-  const location = useLocation()
+  const location = useLocation();
 
   const handleNavigate = (path) => {
-    navigate(path)
-    // setIsOpen(false); // Close the sidebar after navigation
-  }
-
-  if (!isOpen) {
-    return null
-  }
+    navigate(path);
+  };
 
   return (
     <>
-      <div className='sidebar'>
-        <ul>
+      <div className="sidebar">
+        <ul className="sidebar-list">
           {SidebarData.map((val, key) => (
             <li
-              className='row'
-              id={location.pathname === val.path ? 'active' : ''}
+              className="row"
+              id={location.pathname === val.path ? "active" : ""}
               key={key}
               onClick={() => handleNavigate(val.path)}
             >
-              <div className='icon'>{val.icon}</div>
-              <div className='row-name'>{val.name}</div>
+              <div className="icon">{val.icon}</div>
+              <div className="row-name">{val.name}</div>
             </li>
           ))}
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
